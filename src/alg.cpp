c5815531ca6f37de2d873dabf1e1b68635534a8d
laal28 <129623228+laal28@users.noreply.github.com>
2024-04-29 12:31:06 +0300

    Solved

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,27 @@
 // Copyright 2021 NNTU-CS
-
-int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int cbinsearch(int* arr, int size, int value) {
+  int c = 0;
+  int rt = size - 1;
+  for ( int lt = 0; lt <= rt; ) {
+      int mid = lt + (rt - lt) / 2;
+      if ( arr[mid] == value ) {
+          c++;
+          int rt = mid - 1;
+          for ( ;rt >= 0 && arr[rt] == value; rt-- )
+              c++;
+        
+          rt = mid + 1;
+          for ( ;rt < size && arr[rt] == value; rt++ )
+              c++;
+    
+          return c;
+      } else if ( arr[mid] < value )
+          lt = mid + 1;
+      else
+          rt = mid - 1;
+  }
+  if (c == 0) {
+      return 0;
+  }
+  return c;
 }
