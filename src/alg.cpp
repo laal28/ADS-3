31a387f870b8cf3d6ee0a99368e92c52f6372e29
laal28 <129623228+laal28@users.noreply.github.com>
2024-04-29 12:35:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,17 +8,18 @@ int cbinsearch(int* arr, int size, int value) {
           c++;
           int rt = mid - 1;
           for ( ; rt >= 0 && arr[rt] == value; rt-- )
-              c++; 
+              c++;
 
           rt = mid + 1;
           for ( ; rt < size && arr[rt] == value; rt++ )
-              c++; 
+              c++;
 
           return c;
-      } else if ( arr[mid] < value )
+      } else if ( arr[mid] < value ) {
           lt = mid + 1;
-      else
+      } else {
           rt = mid - 1;
+      }
   }
   if (c == 0) {
       return 0;
