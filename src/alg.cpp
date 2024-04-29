797f3d5d68d4a8a8d7101e9adbbb5ff9b65c5f09
laal28 <129623228+laal28@users.noreply.github.com>
2024-04-29 12:33:21 +0300

    Fixed codestyle

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,13 +7,13 @@ int cbinsearch(int* arr, int size, int value) {
       if ( arr[mid] == value ) {
           c++;
           int rt = mid - 1;
-          for ( ;rt >= 0 && arr[rt] == value; rt-- )
-              c++;
-        
+          for ( ; rt >= 0 && arr[rt] == value; rt-- )
+              c++; 
+
           rt = mid + 1;
-          for ( ;rt < size && arr[rt] == value; rt++ )
-              c++;
-    
+          for ( ; rt < size && arr[rt] == value; rt++ )
+              c++; 
+
           return c;
       } else if ( arr[mid] < value )
           lt = mid + 1;
